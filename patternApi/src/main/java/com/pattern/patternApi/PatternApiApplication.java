d77e2c7f327b84239141426d76e15a982230429c
fyxxx2 <kkdn0118@gmail.com>
2025-12-06 16:45:16 +0900

    스프링부트 폴더 추가

---
+++ patternApi/src/main/java/com/pattern/patternApi/PatternApiApplication.java
@@ -0,0 +1,13 @@
+package com.pattern.patternApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternApiApplication.class, args);
+	}
+
+}
---
+++ patternApi/src/test/java/com/pattern/patternApi/PatternApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pattern.patternApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
